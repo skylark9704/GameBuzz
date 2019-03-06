f16efed3bef2fd7396fb6141779f089e7450109a
skylark9704 <vsushant97@gmail.com>
2019-03-06 22:16:03 +0530

    Added Sign Up Modal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,6 +18,14 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  open(content2) {
+    this.modalService.open(content2, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
