d687518fb8205231669f46ca5ddc9279d04d99c6
skylark9704 <vsushant97@gmail.com>
2019-03-07 09:45:14 +0530

    Fixed Navbar and Column Responsiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,17 +12,9 @@ export class HeaderComponent implements OnInit {
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
 
-  open(content2) {
-    this.modalService.open(content2, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+
     });
   }
 
